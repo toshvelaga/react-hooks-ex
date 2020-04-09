5161f9b974a3204710b3fbbaf1c764f6594fe84c
Tosh Velaga <toshvelaga@gmail.com>
2020-04-08 17:09:31 -0700

    added function handlers

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ userIngredients, setUserIngredients ] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => [...prevIngredients, { id: Math.random().toString(), ...ingredient}])
+  }
+
+  const removeIngredientHandler = id => {
+    setUserIngredients(prevIngredients => [...prevIngredients.filter(ingredient => ingredient.id !== id)]) 
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
